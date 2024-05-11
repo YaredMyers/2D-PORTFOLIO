1589c6e0bb264dcda4a6261c2bcb61f2cea450db
Yared Santiago González <38158612+YaredMyers@users.noreply.github.com>
2024-05-11 20:42:55 +0200

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript/TypeScript with React these days.
+    I've made a couple of games in that language. I also use Jest and React Testing Library for testing my apps!. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/YaredMyers" target="_blank">Github</a>!`,
+    "cs-degree": `<a href="https://www.freecodecamp.org/certification/yaredmyers/javascript-algorithms-and-data-structures" target="_blank">This is my FreeCodeCamp degree</a>.
+    I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube or play video-games.`,
+    tv: `That's my TV. I've been watching a lot of series from:
+   <a href="https://www.netflix.com/es/" target="_blank">Netflix</a>, <a href="https://www.hbomax.com/es/es" target="_blank">HBO Max</a>,
+  <a href="https://www.primevideo.com/" target="_blank">Prime</a> (sometimes) and <a href="https://www.youtube.com/" target="_blank">YouTube</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="../public/YaredSGCV2024.pdf" target="_blank">Check it out!</a>
+  Contact me at yared.santiagogonzalez@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are a lot of programming books on my shelves. Some of them are in English (I also speak English btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit from my game portfolio, just close the tab.`,
+};
